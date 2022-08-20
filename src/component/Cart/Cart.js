5781dcdfd0c2f500e28ec4300b6f4d5c009896b8
mohammadali-amd <amidi1380@gmail.com>
2022-08-20 20:01:24 +0430

    add http request
    sending http request
    send data to backend

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,26 +1,48 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react';
 
-import Modal from '../UI/Modal'
-import styles from './Cart.module.css'
-import CartContex from '../../store/cart-context'
-import CartItem from './CartItem'
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
-    const cartCtx = useContext(CartContex)
+    const [isCheckout, setIsCheckout] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [didSubmit, setDidSubmit] = useState(false);
+    const cartCtx = useContext(CartContext);
 
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
-    const hasItems = cartCtx.items.length > 0
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
 
     const cartItemRemoveHandler = (id) => {
         cartCtx.removeItem(id);
     };
 
     const cartItemAddHandler = (item) => {
-        cartCtx.addItem({ ...item, amount: 1 });
+        cartCtx.addItem(item);
     };
 
-    const cartItem = (
-        <ul className={styles['cart-item']}>
+    const orderHandler = () => {
+        setIsCheckout(true);
+    };
+
+    const submitOrderHandler = async (userData) => {
+        setIsSubmitting(true);
+        await fetch('https://react-http-ffb8b-default-rtdb.firebaseio.com/orders.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items,
+            }),
+        });
+        setIsSubmitting(false);
+        setDidSubmit(true);
+        cartCtx.clearCart();
+    };
+
+    const cartItems = (
+        <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) => (
                 <CartItem
                     key={item.id}
@@ -32,22 +54,55 @@ const Cart = (props) => {
                 />
             ))}
         </ul>
-    )
-    return (
-        <Modal onClose={props.onClose}>
-            {cartItem}
-            <div className={styles.total}>
+    );
+
+    const modalActions = (
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick={props.onClose}>
+                Close
+            </button>
+            {hasItems && (
+                <button className={classes.button} onClick={orderHandler}>
+                    Order
+                </button>
+            )}
+        </div>
+    );
+
+    const cartModalContent = (
+        <React.Fragment>
+            {cartItems}
+            <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={styles.actions}>
-                <button className={styles['button--alt']} onClick={props.onClose}>
+            {isCheckout && (
+                <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+            )}
+            {!isCheckout && modalActions}
+        </React.Fragment>
+    );
+
+    const isSubmittingModalContent = <p>Sending order data...</p>;
+
+    const didSubmitModalContent = (
+        <React.Fragment>
+            <p>Successfully sent the order!</p>
+            <div className={classes.actions}>
+                <button className={classes.button} onClick={props.onClose}>
                     Close
                 </button>
-                {hasItems && <button className={styles.button}>Order</button>}
             </div>
-        </Modal>
-    )
-}
+        </React.Fragment>
+    );
 
-export default Cart
+    return (
+        <Modal onClose={props.onClose}>
+            {!isSubmitting && !didSubmit && cartModalContent}
+            {isSubmitting && isSubmittingModalContent}
+            {!isSubmitting && didSubmit && didSubmitModalContent}
+        </Modal>
+    );
+};
+
+export default Cart;
